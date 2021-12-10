6314e96952b61cf45d009b7454d365ea86052921
CharlesElloit <aciforcharles@gmail.com>
2021-12-10 21:26:11 +0300

    added more note and code about none primitive type been mutable

--- Javascript/code/data types.js
+++ Javascript/code/data types.js
@@ -16,3 +16,37 @@ anotherPersonName = "Elloit";
 
 console.log(anotherPersonName); // value of anotherPersonName is Elloit.
 console.log(myName); // value of myName is John.
+
+let name = "Charles";
+name[0] = "D"; // can not change
+console.log(name); // Charles
+
+// None primitive data types
+
+const Person = {
+  name: "Charles",
+  favouriteFood: "Taco",
+  hobby: "Programming",
+  gender: {
+    male: true,
+    female: false,
+  },
+};
+
+// Does shallow copy.
+let PersonLikeCharles = { ...Person };
+
+// Manualing doing deepCopy
+let PersonLikeCharles = { gender: { ...Person.gender } };
+
+// Better way of doing deepCopy without thinking about manually
+// deepCoping every single nested object.
+let PersonLikeCharles = JSON.parse(JSON.stringify(Person));
+
+PersonLikeCharles.hobby = "Gaming";
+PersonLikeCharles.gender.male = false;
+console.log("PersonLikeCharles gender: " + PersonLikeCharles.gender.male);
+
+console.log("PersonLikeCharles hobby: ", PersonLikeCharles.hobby);
+console.log("Person hobby: ", Person.hobby);
+console.log("Person gender: ", Person.gender.male);
